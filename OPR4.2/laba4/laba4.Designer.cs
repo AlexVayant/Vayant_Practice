3e8fbcfe7c209250676634d7c0ab6dd2390206d3
AlexVayant <167333024+AlexVayant@users.noreply.github.com>
2024-04-17 22:23:33 +0700

    Add files via upload

---
+++ OPR4.2/laba4/laba4.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace laba4
+{
+    partial class laba4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(358, 264);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 264);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Cambria", 9F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(9, 24);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(345, 214);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Cambria", 9F);
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 14;
+            this.listBox2.Location = new System.Drawing.Point(358, 10);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(554, 228);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 249);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите время:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 264);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(54, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 9F);
+            this.label2.Location = new System.Drawing.Point(12, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 14);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Часы";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(65, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 9F);
+            this.label4.Location = new System.Drawing.Point(85, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 28);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Минуты\r\n\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 14F);
+            this.label5.Location = new System.Drawing.Point(79, -2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 22);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Пункты отправления";
+            // 
+            // laba4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(923, 344);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "laba4";
+            this.Text = "Trains";
+            this.Load += new System.EventHandler(this.laba4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ OPR4.2/laba4/laba4.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace laba4
+{
+    public partial class laba4 : Form
+    {
+        public List<TRAIN> ride = new List<TRAIN>(10);
+        public string[] name = { "Кемерово", "Томск", "Фрязино", "Майами", "Тимирязево", "Мариинск", "Биробиджан" };
+        public List<string> time = new List<string> { $"0845", "1030", "1315", "1650","2000" };
+        public laba4()
+        {
+            InitializeComponent();
+        }
+        private void laba4_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string path = "trains.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.WriteLineAsync($"{time[rnd.Next(0, 5)]}");
+                    writer.WriteLineAsync($"{name[rnd.Next(0, name.Length)]}");
+                    writer.WriteLineAsync($"{rnd.Next(0, 50)}");
+                }
+            }
+            using (StreamReader reader = new StreamReader(path))
+            {
+                for (int i = 0; i < 31; i++)
+                    ride.Add(new TRAIN(reader.ReadLine(), reader.ReadLine(), Convert.ToInt32(reader.ReadLine())));
+            }
+            foreach (string tm in time)
+                listBox1.Items.Add(tm.Insert(2, ":"));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "" || textBox2.Text =="")
+                {
+                    listBox2.Items.Clear();
+                    listBox2.Items.Add("Введите время отправления!");
+                }
+                else
+                {
+                    listBox2.Items.Clear ();
+                    string text = $"{textBox1.Text}{textBox2.Text}";
+                    int counter = 0;
+                    foreach (TRAIN tm in ride.OrderBy(w => w.place))
+                        if (Convert.ToInt32(text) <= Convert.ToInt32(tm.times))
+                        {
+                            listBox2.Items.Add($"Пункт назначения: {tm.place} Номер рейса: {tm.number} Время отправления: {tm.times.Insert(2, ":")}");
+                            counter += 1;
+                        }
+                    if(counter == 0)
+                    {
+                        listBox2.Items.Clear();
+                        listBox2.Items.Add("После данного времени рейсов не найдено!");
+                    }
+                }
+                if (Convert.ToInt32(textBox1.Text)>=24 || Convert.ToInt32(textBox2.Text) >= 60 || Convert.ToInt32(textBox1.Text) < 0 || Convert.ToInt32(textBox2.Text) < 0 )
+                {
+                    listBox2.Items.Clear();
+                    listBox2.Items.Add("Неверный формат времени");
+                }
+            }
+            catch (FormatException)
+            {
+                listBox2.Items.Clear();
+                listBox2.Items.Add("Неверный формат времени");
+            }
+        }
+    }
+    public class TRAIN
+    {
+        public string place { get; set; }
+        public int number { get; set; }
+        public string times { get; set; }
+        public TRAIN(string times1, string place1, int number1)
+        {
+            this.place = place1;
+            this.number = number1;
+            this.times = times1;
+        }
+    }
+}
